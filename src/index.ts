03d49608fc06c2c7e87816bee85e68c2e8060d87
ThePlenkov <petr.plenkov@gmail.com>
2023-10-10 15:49:28 +0000

    no need to declare global

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,19 @@
-import Utils from './utils'
+import Utils from "./utils";
 
 const greet = (name: string): void => {
-  console.log(`Hello ${name}!`)
-}
+  console.log(`Hello ${name}!`);
+};
 
-const main1 = () => {
-  greet('mahaker')
-  
-  console.log(Utils.add(2, 3))
-  console.log(Utils.sub(0, 5))
-}
+global.main1 = () => {
+  greet("mahaker");
 
-const main2 = () => {
-  greet('Hideaki!')
-  
-  console.log(Utils.add(10, 5))
-  console.log(Utils.sub(10, 5))
-}
+  console.log(Utils.add(2, 3));
+  console.log(Utils.sub(0, 5));
+};
 
-declare let global: any
-global.main1 = main1
-global.main2 = main2
+global.main2 = () => {
+  greet("Hideaki!");
+
+  console.log(Utils.add(10, 5));
+  console.log(Utils.sub(10, 5));
+};
